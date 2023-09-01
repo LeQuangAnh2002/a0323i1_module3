1ea6100330d1ffadf9f53ce0fb33d1e9376b43b4
LeQuangAnh2002 <quangngocbqn@gmail.com>
2023-09-01 20:56:35 +0700

    update

--- CaseStudy/case_study.sql
+++ CaseStudy/case_study.sql
@@ -228,3 +228,58 @@ select H.ma_hop_dong, ngay_lam_hop_dong,ngay_ket_thuc,tien_dat_coc,sum(CT.so_luo
 from hop_dong H left join  hop_dong_chi_tiet CT on H.ma_hop_dong = CT.ma_hop_dong
 group by H.ma_hop_dong
 order by  H.ma_hop_dong;
+
+-- '11.	Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng có ten_loai_khach là “Diamond” và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.'
+select DVDK.ma_dich_vu_di_kem, DVDK.ten_dich_vu_di_kem from dich_vu_di_kem DVDk
+join hop_dong_chi_tiet HDCT on HDCT.ma_dich_vu_di_kem = DVDk.ma_dich_vu_di_kem
+join hop_dong HD on HD.ma_hop_dong = HDCT.ma_hop_dong
+join khach_hang KH on KH.ma_khach_hang = HD.ma_khach_hang
+join loai_khach LK on LK.ma_loai_khach = KH.ma_loai_khach
+where LK.ten_loai_khach ='Diamond' and (KH.dia_chi like '%Vinh' or KH.dia_chi like '%Quảng Ngãi');
+-- '12.	Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), so_dien_thoai (khách hàng), ten_dich_vu, 
+-- so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), tien_dat_coc của tất cả các dịch vụ 
+-- đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.'
+
+select HD.ma_hop_dong,NV.ho_ten,KH.ho_ten,KH.so_dien_thoai,DV.ten_dich_vu, sum(HDCT.so_luong) as so_luong_dich_vu_di_kem  , HD.tien_dat_coc,HD.ngay_lam_hop_dong from hop_dong HD
+ join nhan_vien NV on NV.ma_nhan_vien = HD.ma_nhan_vien
+ join khach_hang KH on KH.ma_khach_hang = HD.ma_khach_hang
+ join dich_vu DV on DV.ma_dich_vu = HD.ma_dich_vu 
+ left join hop_dong_chi_tiet HDCT on HDCT.ma_hop_dong = HD.ma_hop_dong
+where HD.ma_hop_dong in (select ma_hop_dong from hop_dong where quarter(ngay_lam_hop_dong) = 4 and year(ngay_lam_hop_dong) = 2020 ) 
+and 
+HD.ma_hop_dong not in (select ma_hop_dong from hop_dong  where (quarter(ngay_lam_hop_dong) = 1 and year(ngay_lam_hop_dong) = 2021) and (quarter(ngay_lam_hop_dong) = 2 and year(ngay_lam_hop_dong) = 2021))
+group by HD.ma_hop_dong;
+
+-- '13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).'
+select DVDK.ma_dich_vu_di_kem, DVDK.ten_dich_vu_di_kem, sum(HDCT.so_luong) as so_luong_dich_vu_di_kem from dich_vu_di_kem DVDK
+join hop_dong_chi_tiet HDCT on DVDK.ma_dich_vu_di_kem = HDCT.ma_dich_vu_di_kem
+group by  DVDK.ma_dich_vu_di_kem
+having so_luong_dich_vu_di_kem = (
+	select max(tong_so_lan_su_dung) from (
+		select ma_hop_dong_chi_tiet,sum(so_luong) as tong_so_lan_su_dung from hop_dong_chi_tiet
+        group by ma_hop_dong_chi_tiet
+    ) as subquery
+);
+-- '14.	Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. Thông tin hiển thị bao gồm ma_hop_dong, 
+-- ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung (được tính dựa trên việc count các ma_dich_vu_di_kem).'
+select HD.ma_hop_dong,LDV.ten_loai_dich_vu,DVDK.ten_dich_vu_di_kem, count(HDCT.ma_dich_vu_di_kem) as so_lan_su_dung from hop_dong_chi_tiet HDCT
+join hop_dong HD on HD.ma_hop_dong = HDCT.ma_hop_dong
+join dich_vu_di_kem DVDK on DVDK.ma_dich_vu_di_kem = HDCT.ma_dich_vu_di_kem
+join dich_vu DV on HD.ma_dich_vu = DV.ma_dich_vu
+join loai_dich_vu LDV on LDV.ma_loai_dich_vu = DV.ma_loai_dich_vu
+group by HD.ma_hop_dong,DVDK.ten_dich_vu_di_kem
+having so_lan_su_dung = 1;
+
+-- '15.	Hiển thi thông tin của tất cả nhân viên bao gồm 
+-- ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, so_dien_thoai, dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.'
+
+select NV.ma_nhan_vien,NV.ho_ten,TD.ten_trinh_do,BP.ten_bo_phan ,NV.so_dien_thoai, NV.dia_chi from hop_dong HD
+join nhan_vien NV on  HD.ma_nhan_vien = NV.ma_nhan_vien
+join trinh_do TD on TD.ma_trinh_do = NV.ma_trinh_do
+join bo_phan BP on BP.ma_bo_phan = NV.ma_bo_phan
+where NV.ma_nhan_vien in (select HD.ma_nhan_vien from hop_dong HD where year(ngay_lam_hop_dong) between 2020 and 2021 group by ma_nhan_vien having count(*) <= 3)
+
+
+
+
+
